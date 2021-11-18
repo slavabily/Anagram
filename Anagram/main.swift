3e1dddce9f5daf0a6cccb2c44adfe4ab66adcb5b
slava bily <slavabily@me.com>
2021-11-18 10:29:26 +0200

    the string is splitted out into two substrings of equal length

--- Anagram/main.swift
+++ Anagram/main.swift
@@ -8,8 +8,22 @@
 import Foundation
 
 func anagram(s: String) -> Int {
+    let arr = Array(s)
+    var arr1 = [Character]()
+    var arr2 = [Character]()
+    for i in 0..<arr.count {
+        if arr1.count < arr.count / 2 {
+            arr1.append(arr[i])
+        } else {
+            arr2.append(arr[i])
+        }
+    }
+    guard arr1.count == arr2.count else {
+        return -1
+    }
+    print("\(arr1)\n\(arr2)")
      
-    return -1
+    return 0
 }
 
 print(anagram(s: "abccde"))
